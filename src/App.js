502bc056661ca9309af5497b5683684ff1e06274
Nicholas Cox <nick.a.cox@gmail.com>
2020-02-06 16:12:41 -0600

    ghpages config

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from './Components/Footer/Footer'
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL || ''}>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/portfolio" component={Portfolio} />
       </Router>
