b64437484eff1c49249528fc59fe351face64f52
Nicholas Cox <nick.a.cox@gmail.com>
2020-02-06 15:32:17 -0600

    Updates

---
+++ precache-manifest.f6a2f694a6297054eb8a14084a1c22b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ead35b18dac01d2f4a8cbb2e18900e",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "c69975389bfd274f4106",
+    "url": "/porfolio/static/css/main.bbe348fc.chunk.css"
+  },
+  {
+    "revision": "826beda73139779b77e9",
+    "url": "/porfolio/static/js/2.5d0f47b4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/porfolio/static/js/2.5d0f47b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69975389bfd274f4106",
+    "url": "/porfolio/static/js/main.6587679f.chunk.js"
+  },
+  {
+    "revision": "c7a056c14a1d7f9f7d13",
+    "url": "/porfolio/static/js/runtime-main.11414032.js"
+  }
+]);
